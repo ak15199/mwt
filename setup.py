28b8d49fda968c2a662aea85aaa4cf7832589bdf
Alex <alex_w_king@yahoo.com>
2018-02-03 20:29:18 -0600

    Updates to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 setup(name="mwt",
-        version="0.9.0",
+        version="0.9.0b1",
         description="Memoize with timeout",
         long_description=readme(),
         url="https://github.com/ak15199/mwt.git",
+        packages=find_packages(),
         author="Alex King",
         author_email="alex_w_king@yahoo.com",
         license="MIT",
-        packages=["mwt"],
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Build Tools",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            ],
+        keywords="memoize cache python function decorator",
         )
